f177e04dd0e2da94108cb10d4ad0c36687567ab3
iduarte04 <iduarte2@asu.edu>
2021-09-15 12:53:58 -0700

    got something going on first load, crashed on reload;

--- flaskCamera.py
+++ flaskCamera.py
@@ -6,13 +6,13 @@ import frameGenerator as cam
 app = Flask(__name__)
 
 
-@app.route('/view')
+@app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/live_feed')
 def vid_feed():
-    return Response(cam.generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(cam.generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frameGenerator.py
+++ frameGenerator.py
@@ -5,16 +5,17 @@ import cv2
 
 url = 'http://server.argos.vision/video_feed'
 
-
 cam = cv2.VideoCapture(url)
 
-
-
 def generate_frames():
-   while (True):
-    frame = cam.read()
-
-    cv2.imshow('frame',frame)
-
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break   
+    while(True):
+        success, frame = cam.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
+            
+           
+            
