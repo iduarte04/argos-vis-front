4e29c7b935cbab53bd9adc997542960a5e1daf35
iduarte04 <iduarte2@asu.edu>
2021-09-16 11:22:42 -0700

    i think i fixed it with a rough working product

--- frameGenerator.py
+++ frameGenerator.py
@@ -1,21 +1,31 @@
 from logging import debug
 from flask import Flask, render_template, Response
 import numpy as np
+import threading
 import cv2
 
+lock = threading.Lock()
 url = 'http://server.argos.vision/video_feed'
 
-cam = cv2.VideoCapture(url)
 
-def generate_frames():
-    while(True):
-        success, frame = cam.read()  # read the camera frame
-        if not success:
-            break
-        else:
+
+    
+def generate_frames():  
+    while True:
+            cam = cv2.VideoCapture(url)
+            success, frame = cam.read()  # read the camera frame
+            if not success:
+                break
+            
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
-            
-           
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + 
+                   b'\r\n\r\n')
+            cam.release() 
+
+
+        
+
+		
             
